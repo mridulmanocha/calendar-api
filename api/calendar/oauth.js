1bf7bfa9cc6b2851cd252c9e6f140d804a09357d
Mridul Manocha <mridul.manocha22@gmail.com>
2020-04-20 17:27:32 +0530

    nemb

---
+++ api/calendar/oauth.js
@@ -0,0 +1,7 @@
+var google = {
+        clientID : '551744569484-mto8h4n8lqbm9ttqa4sjd90ejinrrnmb.apps.googleusercontent.com',
+        clientSecret : 'VUqM1fReHSCW9Zjg8vlYFtDJ',
+        callbackURL: 'http://localhost:3000/google/callback'
+}
+
+module.exports = google;
